80e02710e9fb25c5aeefd701ed08f543720d19e7
Andy <adnrey.bogdanov2005@mail.ru>
2025-09-16 13:10:41 +0300

    сделал лабу в py_labs

--- chm/main.py
+++ chm/main.py
@@ -6,7 +6,7 @@ def print_like_matrix(a: list, b: list, c: list, p: list, q: list, n: int):
     str_p = [f'{x:.4g}' for x in p]
     str_q = [f'{x:.4g}' for x in q]
     max_len = max(len(s) for s in (str_a + str_b + str_c + str_p + str_q))
-    print(f'max_len: {max_len}')
+    #print(f'max_len: {max_len}')
     for i in range(n):
         print(f'{p[i]:>{max_len}.4g}', end='\t')
         print(f'{q[i]:>{max_len}.4g}', end='\t')
@@ -32,39 +32,19 @@ def extract_from_file(a: list, b: list, c: list, p: list, q: list):
         for row in f:
             ch = row.split()
             ch = [float(i) for i in ch]
-            print(ch)
+            #print(ch)
             try:
-                print(f'i: {i}')
+                #print(f'i: {i}')
                 p.append(ch[0])
                 q.append(ch[1])
-                print(p, q, sep='\t', end='\n')
+                #print(p, q, sep='\t', end='\n')
                 if i > 0: a.append(ch[i-1])
                 b.append(ch[i])
                 c.append(ch[i+1])
                 i += 1
             except IndexError as err:
-                print(err)
+                #print(err)
                 break
-
-
-# def to_matrix(n: int, matrix: list, a: list, b: list, c: list, p: list, q: list):
-#     for i in range(n):
-#         row = []
-#         if i == k-1:
-#             row = p
-#             matrix.append(row)
-#             continue
-#         if i == l-1:
-#             row = q
-#             matrix.append(row)
-#             continue
-#         row.extend([0]*(i-1))
-#         if i > 0: row.append(a[i-1])
-#         row.append(b[i])
-#         if i < n - 1: row.append(c[i])
-#         row.extend([0]*(n-(i+2)))
-#         matrix.append(row)
-
 
 # def go_to_k_on_matrix(k: int, rows: list):
 #     #до k строки приводим к 1 на главной диагонали и 0 под ней и в строке k вплоть до k-1 0
@@ -88,54 +68,39 @@ def extract_from_file(a: list, b: list, c: list, p: list, q: list):
 #                 rows[i][j] -= to_sub
 #         print_matrix(rows)
 
-def go_to_k_on_arrays(a: list, b: list, c: list, p: list, q: list, k: int):
-    for i in range(1, k-1):
-        coef = a[i] / b[i-1]
-        to_divide = b[i-1]
-        b[i-1] /= to_divide
-        if i > 1: a[i-1] /= to_divide
-        if i < k: c[i-1] /= to_divide
-        a[i] -= a[i-1] * coef if i == 1 else b[i-1] * coef
-        b[i] -= b[i-1] * coef if i == 1 else c[i-1] * coef
+def go_to_k_on_arrays(a: list, b: list, c: list, p: list, q: list, n: int):
+    #ext_p, ext_q = list(), list()
+    for i in range(n-2):
+        print(f'\n {i}')
+        if i == 0:
+            cur = p
+        else:
+            next_cur = list()
+        for j in range(1, n-i):
+            try:
+                coef = cur[j] / b[i]
+                print(f'coef: {cur[j+i]} / {b[i]}')
+                b[i+j] -= c[i+j-1] * coef
+                if i == 0:
+                    q[i+j] -= q[i+j-1] * coef
+                a[i + j - 1] -= b[i+j-1] * coef
+                cur[j+i] = 0
+            except ZeroDivisionError:
+                print(f'error in {j}')
+        print_like_matrix(a, b, c, p, q, n)
+        cur = q
 
-# def print_like_matrix(a: list, b: list, c: list, p: list, q: list):
-#     numbers = [a, b, c, p, q]
-#     str_matrix = [[f"{x:.4f}" for x in num] for num in numbers]
-#     max_len = max(len(s) for row in str_matrix for s in row)
-#     for i in range(n):
-#         print(*([0] * (i - 1)), sep='\t')
-#         if i > 0: print(f'{a[i]:>{max_len}.4g}', end='\t')
-#         print(f'{b[i]:>{max_len}.4g}', end='\t')
-#         print(f'{b[i]:>{max_len}.4g}', end='\t')
-#         print(*([0] * (n - (i + 2))), sep='\t')
-#
-# def input_arrays(a: list, b: list, c: list, p: list, q: list):
-#     str_a = input('Введите через пробел массив a: ')
-#     str_b = input('Введите через пробел массив b: ')
-#     str_c = input('Введите через пробел массив c: ')
-#     str_p = input('Введите через пробел массив p: ')
-#     str_q = input('Введите через пробел массив q: ')
-#     a.extend(int(i) for i in str_a.split())
-#     b.extend(int(i) for i in str_b.split())
-#     c.extend(int(i) for i in str_c.split())
-#     p.extend(int(i) for i in str_p.split())
-#     q.extend(int(i) for i in str_q.split())
 
 
 if __name__ == '__main__':
-    #from_file = input('From file? (y/n): ')
     a, b, c, p, q = list(), list(), list(), list(), list()
-    #if from_file == 'y':
     n, k, l = 10, 9, 10
     extract_from_file(a, b, c, p, q)
     print()
     print(p, q, sep='\n', end='\n\n')
     print(a, b, c, sep='\n', end='\n\n')
     print_like_matrix(a, b, c, p, q, n)
-        #go_to_k_on_matrix(k, matrix)
-        #print_matrix(matrix)
-        #матрица не создаётся иду по массивам a, b, c, p, q
-    # elif from_file == 'n':
-    #     n, k, l = int(input('Введите n: ')), int(input('Введите k: ')), int(input('Введите l: '))
-    #     input_arrays(a,b,c,p,q)
-    #     print_like_matrix(a, b, c, p, q)
+    go_to_k_on_arrays(a, b, c, p, q, n)
+    print()
+    print(p, q, sep='\n', end='\n\n')
+    print(a, b, c, sep='\n', end='\n\n')
--- py_labs/main.py
+++ py_labs/main.py
@@ -1,11 +1,11 @@
 import random
-from collections import namedtuple
+from typing import List
 
 def gen_card_base(height: int, width: int, suit: str, cost: str) -> str:
     card_base = ''
     for i in range(height):
         if i == 0:
-            card_base += '/' + '-' * width + '\\\n'
+            card_base += '/' + '=' * width + '\\\n'
         elif i == 1:
             card_base += '|' + ' ' + cost + ' ' * (width - 2) + '|\n'
         elif i == 2:
@@ -17,47 +17,317 @@ def gen_card_base(height: int, width: int, suit: str, cost: str) -> str:
         else:
             card_base += '|' + ' ' * width + '|\n'
     else:
-        card_base += '\\' + '-' * width + '/'
+        card_base += '\\' + '=' * width + '/'
     return card_base
 
-def gen_card_ace(height: int, width: int, suit: str, cost: str) -> str:
+def split_to_rows_lst(string: str) -> List[List[str]]:
+    layers: list = string.split('\n')
+    for i in range(len(layers)):
+        layers[i] = list(layers[i])
+    return layers
+
+def join_lst_to_str(lst: List[List[str]]):
+    for i in range(len(lst)):
+        lst[i] = ''.join(lst[i])
+    card = '\n'.join(lst)
+    return card
+
+def gen_card_ace(suit: str, cost: str, height: int = 13, width: int = 18) -> str:
+    start_h, start_w = 4, 4
     card_base = gen_card_base(height, width, suit, cost)
-    layers = card_base.split('\n')
-    start_h = height // 3
-    start_w = width // 4
-    for j, i in enumerate(range(height - start_h, start_h - 1, -1)):
-        if i == (len(range(height - start_h, start_h - 1, -1)) // 2) + start_h:
-            layers[i] = layers[i][:start_w+j + 1] + (suit + ' ') * 4 + layers[i][start_w + j + 9: ]
+    layers =  split_to_rows_lst(card_base)
+    for j, i in enumerate(range(height - start_h, start_h-1, -1)):
+        if i == start_h:
+            if start_w + j == width // 2:
+                layers[i][start_w + j] = suit
+            else:
+                print(j)
+                for a in range(width-(2 * j + 6)):
+                    layers[i][start_w + j + a] = suit
+        elif i == (height-start_h)//2 + start_h - 1:
+            for q in range(0, 3*j + 1, 2):
+                layers[i][start_w + j + q] = suit
         else:
-            layers[i] = layers[i][:start_w + j] + suit + layers[i][start_w+j+1:]
-            layers[i] = layers[i][: width-start_w - j: -1] + suit + layers[i][width-start_w-j-1: : -1]
-    card_ace = '\n'.join(layers)
-    return card_ace
+            layers[i][start_w + j] = suit
+            layers[i][width - start_w - j] = suit
 
-def gen_card_queen(height: int, width: int, suit: str, cost: str) -> str:
+    card = join_lst_to_str(layers)
+    return card
+
+def gen_card_queen(suit: str, cost: str, height: int = 13, width: int = 18) -> str:
+    start_h, start_w = 4, 4
     card_base = gen_card_base(height, width, suit, cost)
-    layers = card_base.split('\n')
-    start_h = height // 3
-    start_w = width // 4
-    for j, i in enumerate(range(height - start_h, start_h - 1, -1)):
-        if j == 0:
-            layers[i] = layers[i][:start_w] + suit * 3 + layers[i][start_w + 1:]
-    card_queen = '\n'.join(layers)
-    return card_queen
+    layers =  split_to_rows_lst(card_base)
 
+    for j in range(2, width - 2 * start_w - 1, 2):
+        layers[start_h][start_w + j] = suit
+        layers[height - start_h][start_w + j] = suit
+
+    for j in range(1, height - 2 * start_h ):
+        layers[start_h + j][start_w] = suit
+        layers[start_h + j][width - start_w] = suit
+
+    layers[height - start_h][width - start_w + 1] = suit
+
+    card = join_lst_to_str(layers)
+    return card
+
+def gen_card_king(suit: str, cost: str, height: int = 13, width: int = 18):
+    start_h, start_w = 4, (width - 4) // 2
+    card_base = gen_card_base(height, width, suit, cost)
+    layers = split_to_rows_lst(card_base)
+
+    for i in range(start_h, height - start_h + 1):
+        layers[i][start_w] = suit
+
+    for j, i in enumerate(range(height - 2 * start_h + 1, height - start_h + 1), 1):
+        layers[i][start_w + j] = suit
+        layers[height - i][start_w + j] = suit
+
+    card = join_lst_to_str(layers)
+    return card
+
+def gen_card_jack(suit: str, cost: str, height: int = 13, width: int = 18) -> str:
+    start_h, start_w = 4, 4
+    card_base = gen_card_base(height, width, suit, cost)
+    layers = split_to_rows_lst(card_base)
+
+    for i in range(start_h, height - start_h):
+        layers[i][width - 2 * start_w] = suit
+
+    for i in range(start_w + 4, width - start_w, 2):
+        layers[start_h][i] = suit
+
+    layers[height-start_h][width - 2 * start_w - 1] = suit
+    layers[height - start_h -1][width - 2 * start_w - 3] = suit
+
+    card = join_lst_to_str(layers)
+    return card
+
+def gen_card_two(suit: str, cost: str, height: int = 13, width: int = 18) -> str:
+    start_h, start_w = 3, 4
+    card_base = gen_card_base(height, width, suit, cost)
+    layers = split_to_rows_lst(card_base)
+    for i in range(2):
+        layers[start_h + 4*i][width // 2 + 1] = suit
+        layers[start_h + 1 + 4*i][width // 2 + 1] = suit
+        layers[start_h + 2 + 4*i][width // 2 + 1] = suit
+        layers[start_h + 1 + 4*i][width // 2 + 2] = suit
+        layers[start_h + 1 + 4*i][width // 2] = suit
+    card = join_lst_to_str(layers)
+    return card
+
+def gen_card_three(suit: str, cost: str, height: int = 13, width: int = 18) -> str:
+    start_h, start_w = 2, 4
+    card_base = gen_card_base(height, width, suit, cost)
+    layers = split_to_rows_lst(card_base)
+    for i in range(3):
+        layers[start_h + 4*i][width // 2 + 1] = suit
+        layers[start_h + 1 + 4*i][width // 2 + 1] = suit
+        layers[start_h + 2 + 4*i][width // 2 + 1] = suit
+        layers[start_h + 1 + 4*i][width // 2 + 2] = suit
+        layers[start_h + 1 + 4*i][width // 2] = suit
+    card = join_lst_to_str(layers)
+    return card
+
+def gen_card_four(suit: str, cost: str, height: int = 13, width: int = 18) -> str:
+    start_h, start_w = 4, 4
+    card_base = gen_card_base(height, width, suit, cost)
+    layers = split_to_rows_lst(card_base)
+    for i in range(2):
+        layers[start_h + 4*i][start_w + 1] = suit
+        layers[start_h + 1 + 4*i][start_w + 1] = suit
+        layers[start_h + 2 + 4*i][start_w + 1] = suit
+        layers[start_h + 1 + 4*i][start_w + 2] = suit
+        layers[start_h + 1 + 4*i][start_w] = suit
+    for i in range(2):
+        layers[start_h + 4*i][width-start_w-1] = suit
+        layers[start_h + 1 + 4*i][width-start_w-1] = suit
+        layers[start_h + 2 + 4*i][width-start_w-1] = suit
+        layers[start_h + 1 + 4*i][width-start_w-2] = suit
+        layers[start_h + 1 + 4*i][width-start_w] = suit
+    card = join_lst_to_str(layers)
+    return card
+
+def gen_card_five(suit: str, cost: str, height: int = 13, width: int = 18) -> str:
+    start_h, start_w = 4, 4
+    card_base = gen_card_base(height, width, suit, cost)
+    layers = split_to_rows_lst(card_base)
+    layers[start_h][start_w] = suit
+    layers[height - start_h][start_w] = suit
+    layers[start_h][width - start_w] = suit
+    layers[height - start_h][width - start_w] = suit
+    layers[height - 2 * start_h + 1][width - 2 * start_w - 1] = suit
+    card = join_lst_to_str(layers)
+    return card
+
+def gen_card_six(suit: str, cost: str, height: int = 13, width: int = 18) -> str:
+    start_h, start_w = 4, 4
+    card_base = gen_card_base(height, width, suit, cost)
+    layers = split_to_rows_lst(card_base)
+    for i in range(0, 5, 2):
+        layers[start_h + i][start_w] = suit
+
+    for i in range(0, 5, 2):
+        layers[start_h + i][width - start_w] = suit
+
+    card = join_lst_to_str(layers)
+    return card
+
+def gen_card_seven(suit: str, cost: str, height: int = 13, width: int = 18) -> str:
+    start_h, start_w = 4, 4
+    card_base = gen_card_base(height, width, suit, cost)
+    layers = split_to_rows_lst(card_base)
+    for i in range(0, 5, 2):
+        layers[start_h + i][start_w] = suit
+
+    layers[start_h + 1][width//2] = suit
+
+    for i in range(0, 5, 2):
+        layers[start_h + i][width - start_w] = suit
+
+    card = join_lst_to_str(layers)
+    return card
+
+def gen_card_eight(suit: str, cost: str, height: int = 13, width: int = 18) -> str:
+    start_h, start_w = 4, 4
+    card_base = gen_card_base(height, width, suit, cost)
+    layers = split_to_rows_lst(card_base)
+    for i in range(0, 5, 2):
+        layers[start_h + i][start_w] = suit
+
+    layers[start_h + 1][width//2] = suit
+    layers[height - start_h - 2][width // 2] = suit
+
+    for i in range(0, 5, 2):
+        layers[start_h + i][width - start_w] = suit
+
+    card = join_lst_to_str(layers)
+    return card
+
+def gen_card_nine(suit: str, cost: str, height: int = 13, width: int = 18) -> str:
+    start_h, start_w = 4, 4
+    card_base = gen_card_base(height, width, suit, cost)
+    layers = split_to_rows_lst(card_base)
+    for i in range(0, 5, 2):
+        layers[start_h + i - 1][start_w] = suit
+
+    layers[start_h][width//2] = suit
+    layers[height//2][width//2] = suit
+    layers[height - start_h][width // 2] = suit
+
+    for i in range(0, 5, 2):
+        layers[start_h + i - 1][width - start_w] = suit
+
+    card = join_lst_to_str(layers)
+    return card
+
+def gen_card_ten(suit: str, cost: str, height: int = 13, width: int = 18) -> str:
+    start_h, start_w = 4, 4
+    card_base = gen_card_base(height, width, suit, cost)
+    layers = split_to_rows_lst(card_base)
+    for i in range(0, 7, 2):
+        layers[start_h + i - 1][start_w] = suit
+
+    layers[start_h][width//2] = suit
+    layers[height - start_h - 1][width // 2] = suit
+
+    for i in range(0, 7, 2):
+        layers[start_h + i - 1][width - start_w] = suit
+
+    card = join_lst_to_str(layers)
+    return card
+
+def gen_card(suit: str, cost: str) -> str:
+    options = {
+        '2': gen_card_two, '3': gen_card_three, '4': gen_card_four,
+        '5': gen_card_five, '6': gen_card_six, '7': gen_card_seven,
+        '8': gen_card_eight, '9': gen_card_nine, '10': gen_card_ten,
+        'J': gen_card_jack, 'Q': gen_card_queen, 'K': gen_card_king,
+        'A': gen_card_ace
+    }
+    return options.get(cost)(suit, cost)
+
+def from_str_cost_to_int(cost: str, card_sum: int) -> int:
+    options = {}
+    options = options.fromkeys(['J', 'Q', 'K'], 10)
+    options['A'] = 11 if card_sum <= 10 else 1
+    num = options.get(cost)
+    if num is None: num = int(cost)
+    return num
+
+def print_cards(cards):
+    cards_with_layers = [i.split('\n') for i in cards]
+    height, width = 14, 18
+    for i in range(height):
+        for j in range(len(cards_with_layers)):
+            print(cards_with_layers[j][i], end='\t')
+        print()
 
 
 if __name__ == "__main__":
-    # Card = namedtuple('Card', 'cost, suit')
-    # # user = {}
-    # # bot = {}
-    # # while True:
-    # #     user_pick = input('Взять карту (y/n): ')
-    # #     if user_pick == 'y': pass
-    # #     elif user_pick == 'n': pass
-    # #     else:
-    # #         print('Неправильный ввод')
-    height, width = 11, 20
-    c = gen_card_ace(height, width, '♠', 'A')
-    a = gen_card_queen(height, width, '♠', 'Q')
-    print(a)
+    deck = []
+    for i in tuple(range(2, 11)) + ('J', 'Q', 'K', 'A'):
+        for j in ('♠', '♥', '♦', '♣'):
+            deck.append(dict(suit=j, cost=str(i)))
+    random.shuffle(deck)
+    while input('Играем? (y/n): ').lower() == 'y':
+        user = [deck[0], deck[1]]
+        bot = [deck[2], deck[3]]
+        user_sum = 0
+        bot_sum = 0
+        deck = deck[4:]
+        print('user cards: ')
+        for card in user: user_sum += from_str_cost_to_int(card['cost'], user_sum)
+        print_cards(gen_card(card['suit'], card['cost']) for card in user)
+        print(f'user sum: {user_sum}')
+        print('bot cards: ')
+        for card in bot: bot_sum += from_str_cost_to_int(card['cost'], bot_sum)
+        print_cards(gen_card(card['suit'], card['cost']) for card in bot)
+        print(f'bot sum: {bot_sum}')
+        if user_sum == 21 and bot_sum == 21:
+            print('DRAW')
+            continue
+        elif user_sum == 21:
+            print('USER WIN')
+            continue
+        elif bot_sum == 21:
+            print('BOT WIN')
+            continue
+
+        while True:
+            user_lose = False
+            bot_lose = False
+            while input('Брать карту? (y/n): ').lower() == 'y':
+                card = deck[0]
+                user_sum += from_str_cost_to_int(card['cost'], user_sum)
+                user.append(card)
+                deck = deck[1:]
+                print('user cards: ')
+                print_cards(gen_card(card['suit'], card['cost']) for card in user)
+                print(f'user sum: {user_sum}')
+                if user_sum >= 21:
+                    if user_sum > 21: user_lose = True
+                    break
+            if user_lose:
+                print('BOT WIN')
+                break
+            while bot_sum < 21:
+                card = deck[0]
+                bot_sum += from_str_cost_to_int(card['cost'], bot_sum)
+                bot.append(card)
+                deck = deck[1:]
+                print('bot cards: ')
+                print_cards(gen_card(card['suit'], card['cost']) for card in bot)
+                print(f'bot sum: {bot_sum}')
+                if bot_sum > 21:
+                    bot_lose = True
+                    break
+            if bot_lose:
+                print('USER WIN')
+                break
+
+            if user_sum > bot_sum: print('USER WIN')
+            elif bot_sum > user_sum: print('BOT WIN')
+            else: print('DRAW')
+
