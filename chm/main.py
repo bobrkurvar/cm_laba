192e9595a19850642289ca7781c46751e1737dae
bobrkurvar <andrey.bogdanov2005@mail.ru>
2025-09-25 17:28:27 +0300

    почти готова проходит как надо, но значения иксов не те

--- chm/main.py
+++ chm/main.py
@@ -1,12 +1,10 @@
-def print_like_matrix(a: list, b: list, c: list, p: list, q: list, n: int, f: list, ext_arr: dict | None = None):
+def print_like_matrix(a: list, b: list, c: list, p: list, q: list, n: int, f: list | None = None):
     str_a, str_b, str_c, str_p, str_q = ([f'{x:.4g}' for x in a], [f'{x:.4g}' for x in b], [f'{x:.4g}' for x in c], [f'{x:.4g}' for x in p], [f'{x:.4g}' for x in q])
     max_len = max(len(s) for s in (str_a + str_b + str_c + str_p + str_q))
     rows = ''
-    flag = False
-    if ext_arr:
-        ext_arr_index = 0
-        print('EXT_ARR IS EXISTS')
-        flag = True
+    for i in range(n - 1):
+        if c[i] == 0:
+            c[i] = abs(c[i])
     for i in range(n):
         for j in range(n):
             if p[i] == 0: p[i] = abs(p[i])
@@ -17,17 +15,10 @@ def print_like_matrix(a: list, b: list, c: list, p: list, q: list, n: int, f: li
             elif j == i - 1:
                 if a[i-1] == 0: a[i-1] = abs(a[i-1])
                 rows += f'{a[i-1]:>{max_len}.4g}\t'
-            elif flag and ext_arr['row'] <= i and ext_arr['col'] == j:
-                print('EXTRA')
-                try:
-                    rows += f'{ext_arr['arr'][ext_arr_index]:>{max_len}.4g}\t'
-                    ext_arr_index += 1
-                except IndexError:
-                    flag = False
-                    rows += f'{0:>{max_len}.4g}\t'
             else:
                 rows += f'{0:>{max_len}.4g}\t'
-        rows += f'| {f[i]:>{max_len}.4g}\t'
+        if f:
+            rows += f'| {f[i]:>{max_len}.4g}\t'
         rows += '\n'
     print(rows)
 
@@ -54,73 +45,9 @@ def extract_from_file(a: list, b: list, c: list, p: list, q: list, ans: list):
             except IndexError:
                 continue
 
-# def go_down_on_arrays(a: list, b: list, c: list, p: list, q: list, n: int):
-#     cur = None
-#     for i in range(n):
-#         if cur is None:
-#             cur = p
-#         coefs = []
-#         temp = b[i]
-#         b[i] = 1
-#         if i > 0:
-#             a[i - 1] /= temp
-#         else:
-#             p[i] = 1
-#         if i == 1:
-#             q[i] = 1
-#         if i < n-1:
-#             c[i] /= temp
-#         print('На диагонали единицы:', end=' ')
-#         if not (cur is None) and not (cur is q) and not (cur is p):
-#             print('С extra arr')
-#             print_like_matrix(a, b, c, p, q, n, dict(arr=cur[1:], row=i+2, col=i))
-#         else:
-#             print('Без extra arr')
-#             print_like_matrix(a, b, c, p, q, n)
-#         for j in range(n):
-#             if j <= i and (cur is p or cur is q) : continue
-#             try:
-#                 coef = cur[j]
-#                 coefs.append(coef)
-#                 print(f"coef: {cur[j]} / {b[i]} : {coef}")
-#                 print(f'Обнуляется {cur[j]}')
-#                 cur[j] = 0
-#                 if i >= 1:
-#                     a[i-1] = 0
-#             except IndexError:
-#                 break
-#         if i >= 1:
-#             try:
-#                 for pos, z in enumerate((b, a)):
-#                     print(f'{z[i + 1]} - {c[i]} * {coefs[pos]} ({c[i] * coefs[pos]})', sep=' ')
-#                     z[i + 1] -= c[i] * coefs[pos]
-#                     print(f'= {z[i + 1]}')
-#                 cur = [a[i + 1]]
-#             except IndexError: pass
-#             for z in range(2, len(range(i+1, n))):
-#                 try:
-#                     print(f'cur_name: {-1 * coefs[z]} * {c[i]} : {-1 * coefs[z] * c[i]}')
-#                     cur_num = -1 * (coefs[z]) * c[i]
-#                     cur.append(cur_num)
-#                 except IndexError:
-#                     break
-#         else:
-#             cur = q
-#         print(cur)
-#         print('После вычета: ')
-#         if cur is p or cur is q:
-#             print_like_matrix(a, b, c, p, q, n)
-#         else:
-#             print(f"PRINT WITH EXTRA ARRAY WITH ROW {i + 4} AND COLS {i + 1}")
-#             print_like_matrix(a, b, c, p, q, n, dict(arr=cur[1:], row=i+3, col=i + 1))
-#
-# def go_up_on_arrays(c: list):
-#     for i in range(len(c)):
-#         c[i] = 0
-
 def divide_by_diagonal_elem(i, n, a, b, c, p, q, f):
     temp = b[i]
-    print('Делим на %s', temp)
+    print('Делим на ', temp)
     b[i] = 1
     f[i] /= temp
     if i == 0:
@@ -129,6 +56,7 @@ def divide_by_diagonal_elem(i, n, a, b, c, p, q, f):
         q[i] = 1
         b[i - 1] = 1
         p[i - 1] = 1
+        f[i - 1] /= temp
     else:
         p[i] /= temp
         q[i] /= temp
@@ -137,55 +65,83 @@ def divide_by_diagonal_elem(i, n, a, b, c, p, q, f):
     if i < n - 1:
         c[i] /= temp
 
-def sub_to_diagonal_elem_up(i, n, a, b, c, f):
-    coef = c[i - 1] / b[i]
-    print(f'Coef: {c[i - 1]} / {b[i]} : {coef}')
-    print(f'Обнуляется {c[i - 1]}')
-    print(f'f: {f[i-1]} - {f[i]} * ({c[i - 1]} / {b[i]}) ({coef})')
-    f[i - 1] -= f[i] * coef
-    c[i - 1] = 0
-    if i > n - 1:
-        print(f'b - {a[i - 1]} * ({c[i - 1]} / {b[i]}) ({coef})')
-        b[i] -= a[i - 1] * coef
+def sub_to_diagonal_elem_up(i, a, b, c, p, q, f):
+    if c[i - 1] != 0:
+        coef = c[i - 1] / b[i]
+        print(f'Coef: {c[i - 1]} / {b[i]} : {coef}')
+        print(f'Обнуляется {c[i - 1]}')
+        print(f'f: {f[i-1]} - {f[i]} * ({c[i - 1]} / {b[i]}) ({coef})')
+        f[i - 1] -= f[i] * coef
+        print(f'b: {b[i-1]} - {a[i - 1]} * ({c[i - 1]} / {b[i]}) ({coef})')
+        b[i - 1] -= a[i - 1] * coef
+        print(f'p: {p[i - 1]} - {p[i]} * ({c[i - 1]} / {b[i]}) ({coef})')
+        p[i - 1] -= p[i] * coef
+        print(f'q: {q[i - 1]} - {q[i]} * ({c[i - 1]} / {b[i]}) ({coef})')
+        q[i - 1] -= q[i] * coef
+        c[i - 1] = 0
 
-def sub_to_diagonal_elem_down(i, n, b, f):
-    if i < n - 1 and a[i] != 0:
-        coef = a[i] * b[i - 1]
-        print(f'Coef for a: {a[i]} / {b[i]} : {coef}')
-        print(f'Обнуляется {a[i]}')
-        print(f'f: {f[i + 1]} - {f[i]} * ({a[i]} / {b[i]}) ({coef})')
-        f[i + 1] -= f[i] * coef
-        a[i] -= 0
 
+def to_null_on_p(n, p, f, a):
+    for i in range(1, n):
+        coef = p[i]
+        print(f'Coef: {p[i]} / 1')
+        print(f'Обнуляется p: {p[i]}')
+        p[i] = 0
+        if i == 1:
+            print(f'Обнуляется a: {a[i - 1]}')
+            a[i - 1] = 0
+        print(f'f: {f[i]} - {f[i - 1]} * {coef})')
+        f[i] -= f[i - 1] * coef
+
+def to_null_on_q(n, a, q, f):
+    for i in range(2, n):
+        if i == 2: a[i - 1] = 0
+        coef = q[i]
+        print(f'Обнуляется q: {q[i]}')
+        q[i] = 0
+        print(f'Coef: {q[i]} / 1')
+        print(f'f: {f[i]} - {f[i - 1]} * {coef})')
+        f[i] -= f[i - 1] * coef
+
+def to_null_on_another(n, a, f):
+    for i in range(2, n - 1):
+        if a[i] != 0:
+            coef = a[i]
+            print(f'Обнуляется p: {a[i]}')
+            a[i] = 0
+            print(f'Coef: {a[i]} / 1')
+            print(f'f: {f[i]} - {f[i - 1]} * {coef})')
+            f[i] -= f[i - 1] * coef
 
 def go_up(a, b, c, p, q, f, n):
+    print('-' * 50 + 'GO UP' + '-' * 50)
     for i in range(n-1, 0, -1):
         divide_by_diagonal_elem(i, n, a, b, c, p, q, f)
         print_like_matrix(a, b, c, p, q, n, f)
-        sub_to_diagonal_elem_up(i, n, a, b, c, f)
+        sub_to_diagonal_elem_up(i, a, b, c, p, q, f)
         print_like_matrix(a, b, c, p, q, n, f)
-        sub_to_diagonal_elem_down(i, n, b, f)
-        print_like_matrix(a, b, c, p, q, n, f)
-
-# def answer_from_matrix(n: int, f: list) -> list:
-#     x = list()
-#     for i in range(n):
-#         x.append(f[i])
-#     return x
+    print('\n' + '-' * 50 + 'GO DOWN' + '-' * 50, end='\n\n')
+    to_null_on_p(n, p, f, a)
+    to_null_on_q(n, a, q, f)
+    to_null_on_another(n, a, f)
+    print_like_matrix(a, b, c, p, q, n, f)
 
 if __name__ == '__main__':
     a, b, c, p, q, f = list(), list(), list(), list(), list(), list()
+    a2, b2, c2, p2, q2, f2 = list(), list(), list(), list(), list(), list()
     n, k, l = 10, 9, 10
     extract_from_file(a, b, c, p, q, f)
+    extract_from_file(a2, b2, c2, p2, q2, f2)
     print()
     print(p, q, sep='\n', end='\n\n')
     print(a, b, c, f, sep='\n', end='\n\n')
     print_like_matrix(a, b, c, p, q, n, f)
     go_up(a, b, c, p, q, f, n)
-    print_like_matrix(a, b, c, p, q, n, f)
-    #answer_arr = answer_from_matrix(n, f)
     print()
     print(p, q, sep='\n', end='\n\n')
-    print(a, b, c, sep='\n', end='\n\n')
+    print(a, b, c, f, sep='\n', end='\n\n')
+    for i in range(1, n+1):
+        print(f'x{i}: {f[i-1]}')
+    print_like_matrix(a2, b2, c2, p2, q2, n, f2)
     #print(answer_arr)
     #Сделать проходку снизу вверх лишний массив не понадобится даже?
