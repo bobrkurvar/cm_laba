644d77068aa15e3aedf5a822678cc3463311eb37
Andy <adnrey.bogdanov2005@mail.ru>
2025-10-07 13:57:21 +0300

    сделал тесты и таблицу

--- chm/main.py
+++ chm/main.py
@@ -1,4 +1,4 @@
-
+from matrix_generator import generate_system_arrays, generate_system_arrays_without_zero
 
 def print_like_matrix(a: list, b: list, c: list, p: list, q: list, n: int, f: list | None = None):
     str_a, str_b, str_c, str_p, str_q = ([f'{x:.4g}' for x in a], [f'{x:.4g}' for x in b], [f'{x:.4g}' for x in c], [f'{x:.4g}' for x in p], [f'{x:.4g}' for x in q])
@@ -19,7 +19,7 @@ def print_like_matrix(a: list, b: list, c: list, p: list, q: list, n: int, f: li
                 rows += f'{a[i-1]:>{max_len}.4g}\t'
             else:
                 rows += f'{0:>{max_len}.4g}\t'
-        if f:
+        if f is not None:
             rows += f'| {f[i]:>{max_len}.4g}\t'
         rows += '\n'
     print(rows)
@@ -49,21 +49,16 @@ def extract_from_file(file: str, a: list, b: list, c: list, p: list, q: list, an
 
 def divide_by_diagonal_elem(i, a, b, c, p, q, f, f_):
     temp = b[i]
+    #print(f'Деление строки {i} на {temp}')
     a[i - 1] /= temp
     b[i] = 1
     f[i] /= temp
-    f_[i] /= temp
+    if f_: f_[i] /= temp
     p[i] /= temp
     try:
         c[i] /= temp
     except IndexError: pass
-    if i == 1:
-        q[i] = 1
-        p[i - 1] = 1
-        f[i - 1] /= b[i - 1]
-        f_[i - 1] /= b[i - 1]
-        b[i - 1] = 1
-        print()
+    if i == 1: q[i] = 1
     else:
         q[i] /= temp
 
@@ -72,63 +67,72 @@ def divide_by_diagonal_elem(i, a, b, c, p, q, f, f_):
 def sub_to_diagonal_elem_up(i, a, b, c, p, q, f, f_):
     if c[i - 1] != 0:
         coef = c[i - 1] / b[i]
-        print(f'Coef: {c[i - 1]} / {b[i]} : {coef}', end='\t')
-        print(f'Обнуляется {c[i - 1]}', end='; ')
+        #print(f'Coef: {c[i - 1]} / {b[i]} : {coef}', end='\t')
+        #print(f'Обнуляется {c[i - 1]}', end='; ')
         f[i - 1] -= f[i] * coef
-        f_[i - 1] -= f_[i] * coef
-        print(f'b: {b[i-1]} - {a[i - 1] * coef} = {b[i - 1] - a[i - 1] * coef}', end='; ')
+        if f_: f_[i - 1] -= f_[i] * coef
+        #print(f'b: {b[i - 1]} - {a[i-1]} * {coef}: {a[i - 1] * coef} = {b[i - 1] - a[i - 1] * coef}', end='; ')
         b[i - 1] -= a[i - 1] * coef
-        print(f'p: {p[i - 1]} - {p[i] * coef} = {p[i - 1] - p[i] * coef}', end='; ')
+        #print(f'p: {p[i - 1]} - {p[i] * coef} = {p[i - 1] - p[i] * coef}', end='; ')
         p[i - 1] -= p[i] * coef
-        print(f'q: {q[i - 1]} - {q[i] * coef} = {q[i - 1] - q[i] * coef}')
+        #print(f'q: {q[i - 1]} - {q[i] * coef} = {q[i - 1] - q[i] * coef}')
         q[i - 1] -= q[i] * coef
         c[i - 1] = 0
         if i == 2:
             a[0] -= p[i] * coef
         elif i == 3:
             a[1] -= q[i] * coef
+        if i == 1:
+            c[i - 1] /= b[i - 1]
+            q[i - 1] /= b[i - 1]
+    if i == 1:
+        #print_like_matrix(a, b, c, p, q, n, f)
+        #print(f'Деление строки {i - 1} на {b[i - 1]}')
+        if f_: f_[i - 1] /= b[i - 1]
+        f[i - 1] /= b[i - 1]
+        b[i - 1] = p[i - 1] = 1
+
 
 
 def to_null_on_p(n, p, f, a, f_):
     for i in range(1, n):
-        print_like_matrix(a, b, c, p, q, n, f)
+        #print_like_matrix(a, b, c, p, q, n, f)
         coef = p[i]
-        print(f'Coef: {p[i]}', end='\t')
-        print(f'Обнуляется p: {p[i]}', end='\t')
+        #print(f'Coef: {p[i]}', end='\t')
+        #print(f'Обнуляется p: {p[i]}', end='\t')
         p[i] = 0
         if i == 1:
-            print(f'Обнуляется a: {a[i - 1]}', end='\t')
+            #print(f'Обнуляется a: {a[i - 1]}', end='\t')
             a[i - 1] = 0
-        print(f'f: {f[i]} - {f[0]} * {coef}) = {f[i] - f[0] * coef}')
+        #print(f'f: {f[i]} - {f[0]} * {coef}) = {f[i] - f[0] * coef}')
         f[i] -= f[0] * coef
-        f_[i] -= f_[0] * coef
+        if f_: f_[i] -= f_[0] * coef
 
 def to_null_on_q(n, a, q, f, f_):
     for i in range(2, n):
-        print_like_matrix(a, b, c, p, q, n, f)
+        #print_like_matrix(a, b, c, p, q, n, f)
         if i == 2: a[i - 1] = 0
         coef = q[i]
-        print(f'Обнуляется q: {q[i]}')
+        #print(f'Обнуляется q: {q[i]}')
         q[i] = 0
-        print(f'Coef: {coef}')
-        print(f'f: {f[i]} - {f[1]} * {coef}) = {f[i] - f[1] * coef}')
+        #print(f'Coef: {coef}')
+        #print(f'f: {f[i]} - {f[1]} * {coef}) = {f[i] - f[1] * coef}')
         f[i] -= f[1] * coef
-        f_[i] -= f_[1] * coef
+        if f_: f_[i] -= f_[1] * coef
 
 def to_null_on_another(n, a, f, f_):
     for i in range(2, n - 1):
         if a[i] != 0:
             coef = a[i]
-            print(f'i={i} Обнуляется p: {a[i]}')
+            #print(f'i={i} Обнуляется p: {a[i]}')
             a[i] = 0
-            print(f'Coef: {coef}', end='\t')
-            print(f'f: {f[i+1]} - {f[i]} * {coef}) = {f[i + 1] - f[i] * coef}')
+            #print(f'Coef: {coef}', end='\t')
+            #print(f'f: {f[i+1]} - {f[i]} * {coef}) = {f[i + 1] - f[i] * coef}')
             f[i + 1] -= f[i] * coef
-            f_[i + 1] -= f_[i] * coef
+            if f_: f_[i + 1] -= f_[i] * coef
 
-def fill_f_(a, b, c, p, q, n):
+def fill_f_(a, b, c, p, q, n, f_):
     for i in range(n-1, -1, -1):
-        print(f'to_f_{i+1} = {b[i]} + {a[i-1]} = {b[i] + a[i-1]}')
         to_f_ = b[i]
         if i > 0:
             to_f_ += a[i-1]
@@ -140,25 +144,22 @@ def fill_f_(a, b, c, p, q, n):
             to_f_ += c[i]
         f_.append(to_f_)
 
-def go_up(a, b, c, p, q, f, n, f_):
-    print('-' * 50 + 'GO UP' + '-' * 50)
+def go_up(a, b, c, p, q, f, n, f_ = None):
+    #print('-' * 50 + 'GO UP' + '-' * 50)
     for i in range(n-1, 0, -1):
-        print_like_matrix(a, b, c, p, q, n, f)
-        print_like_matrix(a, b, c, p, q, n, f_)
+        #print_like_matrix(a, b, c, p, q, n, f)
         divide_by_diagonal_elem(i, a, b, c, p, q, f, f_)
+        #print_like_matrix(a, b, c, p, q, n, f)
         sub_to_diagonal_elem_up(i, a, b, c, p, q, f, f_)
-    print_like_matrix(a, b, c, p, q, n, f)
-    print('\n' + '-' * 50 + 'GO DOWN' + '-' * 50, end='\n\n')
+    #print_like_matrix(a, b, c, p, q, n, f)
+    #print('\n' + '-' * 50 + 'GO DOWN' + '-' * 50, end='\n\n')
     to_null_on_p(n, p, f, a, f_)
-    print_like_matrix(a, b, c, p, q, n, f)
+    #print_like_matrix(a, b, c, p, q, n, f)
     to_null_on_q(n, a, q, f, f_)
-    print_like_matrix(a, b, c, p, q, n, f)
+    #print_like_matrix(a, b, c, p, q, n, f)
     to_null_on_another(n, a, f, f_)
-    print_like_matrix(a, b, c, p, q, n, f)
+    #print_like_matrix(a, b, c, p, q, n, f)
 
-# def gen_set_of_matrix():
-#     for i in range(1, 3):
-#         size_matrix = 10 ** i
 
 
 if __name__ == '__main__':
@@ -166,7 +167,7 @@ if __name__ == '__main__':
     n = 10
     file = "data.txt"
     extract_from_file(file, a, b, c, p, q, f)
-    fill_f_(a, b, c, p, q, n)
+    fill_f_(a, b, c, p, q, n, f_)
     f_ = f_[::-1]
     print(f'f~:{f_}', end='\n\n')
     print(p, q, sep='\n', end='\n\n')
@@ -181,4 +182,68 @@ if __name__ == '__main__':
         print(f'x~{i}: {f_[i-1]:.4g}')
     d_ = max((abs(1 - i) for i in f_))
     print(f'для x~: {d_}')
-    print('-' * 25 + 'TEST' + '-' * 25)
+
+    sizes = [10, 100, 1000]
+    coef_ranges = [(-10, 10), (-100, 100), (-1000, 1000)]
+    sets_per_range = 5
+    _q = 1e-12
+
+    test_counter = 1
+
+    # Таблица результатов
+    results = []
+
+    for n in sizes:
+        for coef_range in coef_ranges:
+            rel_errors_all_sets = []
+            max_errors_all_sets = []
+
+            print(f"\n=== Размер {n}, диапазон {coef_range} ===\n")
+            for set_idx in range(sets_per_range):
+                print('-' * 20 + f'TEST {set_idx + 1}' + '-' * 20)
+                a, b, c, p, q, f, x = generate_system_arrays_without_zero(n, coef_range)
+                print("f:", " ".join(f"{val:.4g}" for val in f))
+                print("___x:", " ".join(f"{i:.4g}" for i in x))
+
+                # Решение системы
+                go_up(a, b, c, p, q, f, n)
+                print("my_x:", " ".join(f"{i:.4g}" for i in f))
+
+                # Вычисление относительных погрешностей
+                delta_list = []
+                for xi, xi_hat in zip(x, f):
+                    if abs(xi) > _q:
+                        delta_list.append(abs(xi - xi_hat) / abs(xi))
+                    else:
+                        delta_list.append(abs(xi - xi_hat))
+
+                delta_max = max(delta_list)
+                delta_mean = sum(delta_list) / len(delta_list)
+
+                print("Средняя относительная погрешность набора: ", delta_mean)
+                print("Оценка точности набора: ", delta_max)
+
+                # Сохраняем для усреднения по диапазону
+                rel_errors_all_sets.append(delta_mean)
+                max_errors_all_sets.append(delta_max)
+
+            # Средние значения по 5 наборам
+            avg_rel_error = sum(rel_errors_all_sets) / sets_per_range
+            avg_max_error = sum(max_errors_all_sets) / sets_per_range
+
+            # Сохраняем в таблицу
+            results.append({
+                "№ теста": test_counter,
+                "Размерность системы": n,
+                "Диапазон значений элементов": coef_range,
+                "Средняя относительная погрешность системы": avg_rel_error,
+                "Среднее значение оценки точности": avg_max_error
+            })
+            test_counter += 1
+
+    # Печать итоговой таблицы
+    print("\n=== Итоговая таблица ===\n")
+    print(f"{'№':<5} {'Размер':<10} {'Диапазон':<20} {'Средн. δ':<25} {'Средн. оценка точности':<25}")
+    for r in results:
+        print(f"{r['№ теста']:<5} {r['Размерность системы']:<10} {str(r['Диапазон значений элементов']):<20} "
+              f"{r['Средняя относительная погрешность системы']:<25.4e} {r['Среднее значение оценки точности']:<25.4e}")
--- chm/matrix_generator.py
+++ chm/matrix_generator.py
@@ -1,118 +1,118 @@
 import numpy as np
-import os
-
 
 def generate_nice_solution(n):
-    """Генерация 'красивого' решения"""
-    # Расширенный список красивых чисел
-    nice_numbers = []
-
-    # Целые числа от -8 до 8
+    nice_numbers = set()
     for i in range(-8, 9):
-        nice_numbers.append(i)
+        nice_numbers.add(float(i))
+    for denom in [2, 4, 8, 16]:
+        for num in range(-8*denom, 8*denom + 1):
+            if num % denom != 0:
+                val = num / denom
+                if abs(val) <= 8:
+                    nice_numbers.add(val)
+    return np.random.choice(sorted(list(nice_numbers)), n, replace=True)
 
-    # Простые дроби с знаменателями 2, 3, 4
-    for denom in [2, 3, 4]:
-        for num in range(-4 * denom, 4 * denom + 1):
-            if num % denom != 0 and abs(num / denom) <= 8:
-                nice_numbers.append(num / denom)
-
-    # Убираем дубликаты и сортируем
-    nice_numbers = sorted(list(set(nice_numbers)))
-
-    # Выбираем n случайных красивых чисел
-    x_exact = np.random.choice(nice_numbers, n, replace=True)
-    return x_exact
-
-
-def generate_tridiagonal_system(n, coef_range):
-    """Генерация системы с красивым решением"""
+def generate_system_arrays(n, coef_range=(-10,10)):
     min_coef, max_coef = coef_range
-
-    # Генерация красивого решения
     x_exact = generate_nice_solution(n)
 
-    # Генерация компонентов матрицы как целых чисел
-    b = np.random.randint(min_coef, max_coef + 1, n)  # главная диагональ
-    a = np.random.randint(min_coef, max_coef + 1, n - 1)  # под главной диагональю
-    c = np.random.randint(min_coef, max_coef + 1, n - 1)  # над главной диагональю
-    p = np.random.randint(min_coef, max_coef + 1, n)  # первый столбец
-    q = np.random.randint(min_coef, max_coef + 1, n)  # второй столбец
+    # главная диагональ, не может быть 0
+    b = np.random.randint(min_coef, max_coef+1, n)
+    b[b==0] = 1
+    b = b.astype(float)
 
-    # Создание полной матрицы
-    A = np.zeros((n, n))
+    # поддиагональ a
+    a = np.random.randint(min_coef, max_coef+1, n-1).astype(float)
+    # наддиагональ c
+    c = np.random.randint(min_coef, max_coef+1, n-1).astype(float)
+
+    # первый и второй столбцы (случайные)
+    p = np.random.randint(min_coef, max_coef+1, n).astype(float)
+    q = np.random.randint(min_coef, max_coef+1, n).astype(float)
+
+    # фиксированные пересечения
+    p[0] = b[0]
+    p[1] = a[0]
+    q[0] = c[0]
+    if n > 1:
+        q[1] = b[1]
+    if n > 2:
+        q[2] = a[1]
+
+    # создаём матрицу для вычисления f
+    A = np.zeros((n,n), dtype=float)
     for i in range(n):
-        A[i, i] = b[i]  # главная диагональ
-        A[i, 0] = p[i]  # первый столбец
-        A[i, 1] = q[i]  # второй столбец
-        if i < n - 1:
-            A[i + 1, i] = a[i]  # под диагональю
-        if i < n - 1:
-            A[i, i + 1] = c[i]  # над диагональю
+        A[i,i] = b[i]  # главная диагональ
+        A[i,0] = p[i]  # первый столбец
+        if n > 1:
+            A[i,1] = q[i]  # второй столбец
+        if i < n-1:
+            A[i+1,i] = a[i]  # поддиагональ
+        if i < n-1:
+            A[i,i+1] = c[i]  # наддиагональ
 
-    # Вычисление правой части
     f = A @ x_exact
 
-    return b, a, c, p, q, f, x_exact
+    return a.tolist(), b.tolist(), c.tolist(), p.tolist(), q.tolist(), f.tolist(), x_exact.tolist()
 
 
-def save_system(b, a, c, p, q, f, x_exact, filename):
-    """Сохранение данных массивов в файл"""
-    with open(filename, 'w') as file:
+def generate_system_arrays_without_zero(n, coef_range=(-10,10)):
+    min_coef, max_coef = coef_range
+    x_exact = generate_nice_solution(n)
 
-        # Записываем векторы (каждый с новой строки)
-        file.write(" ".join(f"{int(val)}" for val in b) + "\n")
-        file.write(" ".join(f"{int(val)}" for val in a) + "\n")
-        file.write(" ".join(f"{int(val)}" for val in c) + "\n")
-        file.write(" ".join(f"{int(val)}" for val in p) + "\n")
-        file.write(" ".join(f"{int(val)}" for val in q) + "\n")
-        file.write(" ".join(f"{val:.6f}" for val in f) + "\n")
-        file.write(" ".join(f"{val:.6f}" for val in x_exact) + "\n")
+    # поддиагональ a
+    a = np.random.randint(min_coef, max_coef+1, n-1).astype(float)
+    # наддиагональ c
+    c = np.random.randint(min_coef, max_coef+1, n-1).astype(float)
 
+    # главная диагональ b, делаем диагонально доминирующей
+    b = np.zeros(n, dtype=float)
+    for i in range(n):
+        lower = abs(a[i-1]) if i > 0 else 0
+        upper = abs(c[i]) if i < n-1 else 0
+        b[i] = lower + upper + np.random.uniform(1,5)  # запас
+        # случайный знак
+        if np.random.rand() > 0.5:
+            b[i] *= -1
 
-# Основная программа
-orders = [10, 100, 1000]
-ranges = [(-10, 10), (-100, 100), (-1000, 1000)]
-repetitions = 1
+    # первый и второй столбцы (случайные)
+    p = np.random.randint(min_coef, max_coef+1, n).astype(float)
+    q = np.random.randint(min_coef, max_coef+1, n).astype(float)
 
-os.makedirs("nice_matrices", exist_ok=True)
+    # фиксированные пересечения
+    p[0] = b[0]
+    if n > 1:
+        p[1] = a[0]
+    q[0] = c[0]
+    if n > 1:
+        q[1] = b[1]
+    if n > 2:
+        q[2] = a[1]
 
-print("Генерация 45 систем с красивыми решениями...")
-file_count = 0
+    # создаём матрицу для вычисления f
+    A = np.zeros((n,n), dtype=float)
+    for i in range(n):
+        A[i,i] = b[i]  # главная диагональ
+        A[i,0] = p[i]  # первый столбец
+        if n > 1:
+            A[i,1] = q[i]  # второй столбец
+        if i < n-1:
+            A[i+1,i] = a[i]  # поддиагональ
+        if i < n-1:
+            A[i,i+1] = c[i]  # наддиагональ
 
-for n in orders:
-    for coef_range in ranges:
-        for rep in range(repetitions):
-            file_count += 1
+    f = A @ x_exact
 
-            # Генерация системы
-            b, a, c, p, q, f, x_exact = generate_tridiagonal_system(n, coef_range)
+    return a.tolist(), b.tolist(), c.tolist(), p.tolist(), q.tolist(), f.tolist(), x_exact.tolist()
 
-            # Сохранение системы
-            filename = f"system_{file_count:02d}_n{n}_range{coef_range[0]}_{coef_range[1]}_rep{rep + 1}.txt"
-            full_path = f"nice_matrices/{filename}"
-            save_system(b, a, c, p, q, f, x_exact, full_path)
+if __name__ == "__main__":
+    n = 10
+    a,b,c,p,q,f,x = generate_system_arrays(n)
 
-            # Показываем примеры решений для первых нескольких систем каждого типа
-            if rep == 0:  # только первый повтор каждого типа
-                print(f"Файл {file_count:02d}: n={n}, диапазон={coef_range}")
-                print(f"  Пример решения (первые 5 элементов): {[f'{val:g}' for val in x_exact[:5]]}")
-
-print(f"\nСоздано {file_count} файлов в папке 'nice_matrices'")
-print("Информация о всех системах сохранена в 'nice_matrices/systems_info.txt'")
-
-# Покажем полные примеры нескольких решений
-print("\nПолные примеры красивых решений:")
-print("=" * 50)
-
-# Пример для n=10
-b, a, c, p, q, f, x_exact = generate_tridiagonal_system(10, (-10, 10))
-print(f"Пример решения (n=10): {[f'{val:g}' for val in x_exact]}")
-
-# Пример для n=100 (первые 10 элементов)
-b, a, c, p, q, f, x_exact = generate_tridiagonal_system(100, (-100, 100))
-print(f"Пример решения (n=100, первые 10): {[f'{val:g}' for val in x_exact[:10]]}")
-
-# Пример для n=1000 (первые 5 элементов)
-b, a, c, p, q, f, x_exact = generate_tridiagonal_system(1000, (-1000, 1000))
-print(f"Пример решения (n=1000, первые 5): {[f'{val:g}' for val in x_exact[:5]]}")
+    print("a:", a)
+    print("b:", b)
+    print("c:", c)
+    print("p:", p)
+    print("q:", q)
+    print("x_exact:", " ".join(f"{v:.4f}" for v in x))
+    print("f:", " ".join(f"{v:.4f}" for v in f))
