3345bc6e8f0f262347f9895fb665a0989672247c
Andy <adnrey.bogdanov2005@mail.ru>
2025-10-22 18:47:40 +0300

    добвил метод toss_card

--- py_laba_2/user.py
+++ py_laba_2/user.py
@@ -14,6 +14,8 @@ class Game:
         shuffle(deck)
         self.deck = deck
         self.users = users
+        self.current_card = None
+        self.turn = None
 
     def give_cards(self):
         for user in self.users:
@@ -24,43 +26,56 @@ class Game:
     def match_cards(card1: SimpleCard, card2: SimpleCard):
         return card1.value == card2.value or card1.color == card2.color
 
+    def toss_card(self, card_index: int = 0, match: bool = True):
+        if not match or self.match_cards(self.current_card, self.users[self.turn].deck[int(card_index)]):
+            self.current_card = self.users[self.turn].deck.pop(card_index)
+            self.deck.append(self.current_card)
+
+    def table_info(self):
+        print("table deck length:", len(self.deck))
+        print(f"turn User {self.turn}")
+        for i, user in enumerate(self.users):
+            print(f"User {i} deck length: {len(user.deck)}")
+
+
     def start(self):
         shuffle(self.users)
         self.give_cards()
-        turn = randint(0, len(self.users) - 1)
-        current_card = self.users[turn].deck.pop(0)
-        flag = True
-        i = turn + 1 if turn + 1 < len(self.users) else 0
-        while flag:
-            print(f"User {i} your turn\ncard on the table: {current_card}")
-            print(f"your cards: {", ".join(str(card) for card in self.users[i].deck)}")
+        self.turn = randint(0, len(self.users) - 1)
+        self.toss_card(match=False)
+        game_flag = True
+        self.turn = self.turn + 1 if self.turn + 1 < len(self.users) else 0
+        while game_flag:
+            print(f"User {self.turn} your turn\ncard on the table: {self.current_card}")
+            print(f"your cards: {", ".join(str(card) for card in self.users[self.turn].deck)}")
             input_flag = True
-            while input_flag and (info := input("выберете действие: 1 - бросить карту, 2 - взять новую карту, 3 - UNO: ")):
+            while input_flag and (info := input("выберете действие: 1 - бросить карту, 2 - взять новую карту, 3 - UNO 4 - tale_info: ")):
                 if info == "1":
-                    print(f"card on the table: {current_card}")
-                    cards_for = [str(i) + " - ( " + str(card) + " )" for i, card in enumerate(self.users[i].deck)]
+                    print(f"card on the table: {self.current_card}")
+                    cards_for = [str(i) + " - ( " + str(card) + " )" for i, card in enumerate(self.users[self.turn].deck)]
                     card_index = input(f"выберите какую карту бросить: {", ".join(cards_for)}: ")
-                    if self.match_cards(current_card, self.users[i].deck[int(card_index)]):
-                        new_card = self.users[i].deck.pop(int(card_index))
-                        self.deck.append(new_card)
-                        current_card = new_card
+                    if self.match_cards(self.current_card, self.users[self.turn].deck[int(card_index)]):
+                        self.toss_card(card_index = int(card_index))
                         input_flag = False
                     else:
                         print("Нельзя положить данную карту на текущую на столе")
                 elif info == "2":
                     new_card = self.deck.pop(0)
                     print("взяли карту: ", new_card)
-                    self.users[i].deck.append(new_card)
+                    self.users[self.turn].deck.append(new_card)
+                    self.toss_card(card_index=len(self.users[self.turn].deck) - 1)
                     input_flag = False
-                elif info == "UNO":
-                    input_flag = False
+                elif info == "3":
+                    if len(self.users[self.turn].deck) == 1:
+                        print(f"UNO BY USER {self.turn}")
+                        input_flag = False
+                elif info == "4":
+                    self.table_info()
                 else:
                     pass
-            if i < len(self.users) - 1:
-                i += 1
-            else:
-                i = 0
+            self.turn = self.turn + 1 if self.turn < len(self.users) - 1 else 0
             print()
+
 
 
 
