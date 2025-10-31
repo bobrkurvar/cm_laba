38b98049766728a072e9742c8587223a28bbddf3
Andy <adnrey.bogdanov2005@mail.ru>
2025-10-31 07:48:20 +0300

    right x

--- chm2/main.py
+++ chm2/main.py
@@ -1,5 +1,5 @@
 from random import randint
-
+import math
 
 def get(i, j, band):
     if i < j:
@@ -23,6 +23,7 @@ def matrix_generate(n: int, l: int):
             row.append(elem)
             j += 1
         result.append(row)
+        row[-1] += n * 10
 
     b = [0] * n
     for i in range(n):
@@ -31,14 +32,51 @@ def matrix_generate(n: int, l: int):
     return result, x, b
 
 
-def print_like_matrix(band: list[list[int]], n: int, f: list[int]):
+def print_like_matrix(band: list[list[int]], n: int, f: list[int] | None = None):
     flat = [f"{x:.4g}" for row in band for x in row]
     width = max(len(x) for x in flat)
 
     for i in range(n):
         row = [f"{get(i, j, band):>{width}.4g}" for j in range(n)]
         print('\t'.join(row), end='')
-        print(f" | {f[i]}")
+        if f:
+            print(f" | {f[i]}")
+        else: print()
+
+def set_in_band(i, j, band, value):
+    if i < j:
+        i, j = j, i
+    d = i - j
+    if d < len(band[i]):
+        band[i][-d - 1] = value
+
+
+def cholesky_band(A):
+    n = len(A)
+    L = [[0.0] * len(A[i]) for i in range(n)]
+
+    for i in range(n):
+        # граница для ленты
+        j_start = max(0, i - (len(A[i]) - 1))
+
+        for j in range(j_start, i + 1):
+            # вычисляем сумму s = Σ_k L[i,k] * L[j,k]
+            s = 0.0
+            k_start = max(0, j - (len(L[j]) - 1))
+            for k in range(k_start, j):
+                s += get(i, k, L) * get(j, k, L)
+
+            val = get(i, j, A) - s
+
+            if i == j:
+                # диагональный элемент
+                if val <= 0:
+                    raise ValueError(f"Матрица не положительно определена: diag[{i}]={val}")
+                set_in_band(i, j, L, math.sqrt(val))
+            else:
+                # поддиагональный элемент
+                set_in_band(i, j, L, val / get(j, j, L))
+    return L
 
 def solve_from_L_band(L: list[list[float]], f: list[float]) -> list[float]:
     n = len(L)
@@ -79,9 +117,11 @@ def main():
     A, x, f = matrix_generate(n, l)
     print(A)
     print(x)
-    print(f)
+    print(f, end='\n\n')
+    L = cholesky_band(A)
+    print_like_matrix(L, n)
     print_like_matrix(A, n, f)
-    result = solve_from_L_band(A, f)
+    result = solve_from_L_band(L, f)
     print(result)
 
 if __name__ == "__main__":
